1484fca5b6e07a564a93b1980ef2a6f6e292fcfb
Bob King <bob@someotherdude.com>
2017-08-15 11:09:32 -0400

    blur bg, preload bg image & transition, change color pallette

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-//$(".wait-to-load").animate({".wait-to-load", "opacity"} 1000);
+var imgUrl = 'http://bking39116-webdev.s3.amazonaws.com/randomQuotes/img/dan-carlson-141263-bg.jpg';
+
+
+$('img#beacon').load( function(){
+  console.log('loaded');
+}).attr('src', imgUrl);
+
+$('body').css('background-image', 'url(' + imgUrl + ')');
